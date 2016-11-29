2a36c3a4657fb6f3b4af0dd78d86d08772d48439
Bogdan <bogdanlitvin8@gmail.com>
2016-11-29 13:29:10 +0200

    [+] add calculating the number of visits

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,9 +1,11 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.page(params[:page]).per(30)
+    @songs = Song.order(:created_at).page(params[:page]).per(30)
   end
 
   def show
     @song = Song.find(params[:id])
+    @song.count_views += 1
+    @song.save
   end
 end
