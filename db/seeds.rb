0e067e4ffacc1df51ad95f469ac7312c9d2ea9e6
bogdan8 <bogdanlitvin8@gmail.com>
2016-12-08 13:54:28 +0200

    [~] change admins

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 unless User.with_any_role(:admin).any?
   admin = User.new
-  admin.email = 'admin@admin.com'
-  admin.password = '123456'
+  admin.email = 'bogdanlitvin8@gmail.com'
+  admin.password = '70sunogo'
   admin.confirmed_at = '2016-07-17 07:35:55.139051'
   admin.confirmation_sent_at = '2016-07-17 07:35:36.292019'
   admin.save
