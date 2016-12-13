2b32f8fd6b605900e487f7d6be14bc62fb6065e5
Bogdan <bogdanlitvin8@gmail.com>
2016-12-13 11:53:13 +0200

    [~] bug fix

--- app/workers/song_index_worker.rb
+++ app/workers/song_index_worker.rb
@@ -5,6 +5,6 @@ class SongIndexWorker
   def perform(song)
     song = Song.find(song)
     song.index!
-    UserMailer.send_add_song(@song.user, @song).deliver_now!
+    UserMailer.send_add_song(song.user, song).deliver_now!
   end
 end
