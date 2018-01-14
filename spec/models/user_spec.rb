6f33214485adf3bdb4b9b3257e3625ae0e6022a9
Bohdan <bogdanlitvin8@gmail.com>
2018-01-14 13:07:00 +0200

    [+] add test for user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,39 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :inet
+#  last_sign_in_ip        :inet
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  confirmation_sent_at   :datetime
+#  unconfirmed_email      :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  provider               :string
+#  uid                    :string
+#  name                   :string
+#
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe '#validations' do
+    it { should validate_presence_of :name }
+  end
+
+  describe '#relations' do
+    it { should have_many :roles }
+    it { should have_many :likes }
+    it { should have_many :songs }
+  end
+end
