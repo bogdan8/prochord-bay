29f576d042b610a82e4b31cf1af72f4d0951907d
Bohdan <bogdanlitvin8@gmail.com>
2018-02-04 17:49:42 +0200

    [+] added facebook Analytics and updated bundle

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 # rails
 gem 'pg', '~> 0.18'
-gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
+gem 'rails', '~> 5.0.0'
 gem 'slim-rails'
 gem 'uglifier', '>= 1.3.0'
 
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,23 +1,24 @@
 module Users
   class RegistrationsController < Devise::RegistrationsController
     before_action :configure_permitted_parameters
-    prepend_before_action :check_captcha, only: %i[create update]
+    prepend_before_action :check_captcha, only: %i[create]
 
-    def create; end
+    def create
+      super
+    end
 
-    def update; end
-
-    private
+    protected
 
     def check_captcha
       return if verify_recaptcha
       self.resource = resource_class.new sign_up_params
+      resource.validate
       respond_with_navigational(resource) { render :new }
     end
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: %I[name])
+      devise_parameter_sanitizer.permit(:account_update, keys: %i[name])
     end
   end
 end
